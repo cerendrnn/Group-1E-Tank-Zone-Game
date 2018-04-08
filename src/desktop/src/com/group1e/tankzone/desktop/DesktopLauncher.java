a2a7db1268dce459d1d63bb2aa94d17410043a0a
PythEch <pythech.tr@gmail.com>
2018-04-08 23:46:25 +0300

    Set resolution to something bigger

--- src/desktop/src/com/group1e/tankzone/desktop/DesktopLauncher.java
+++ src/desktop/src/com/group1e/tankzone/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.group1e.tankzone.Managers.GameManager;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1920;
+		config.height = 1080;
 		new LwjglApplication(new GameManager(), config);
 	}
 }
