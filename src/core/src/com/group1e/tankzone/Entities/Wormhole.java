610779a70424b63f9d18093bd791855e8ba31731
Aybüke Ceren Duran <36036370+cerendrnn@users.noreply.github.com>
2018-05-01 10:24:34 +0300

    Add files via upload

---
+++ src/core/src/com/group1e/tankzone/Entities/Wormhole.java
@@ -0,0 +1,16 @@
+package com.group1e.tankzone.Entities;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.group1e.tankzone.Components.*;
+
+import static com.badlogic.gdx.math.MathUtils.random;
+
+public class Wormhole extends Entity{
+
+    public Wormhole(float pos1_x,float pos1_y,float pos2_x,float pos2_y){
+
+          //texture???
+          this.addComponent(new DamageComponent(1000, false));
+          this.addComponent(new GraphicsComponent());
+    }
+}
