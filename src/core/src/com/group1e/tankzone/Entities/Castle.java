f43226ccb64eaf5c155b306b2e7813607d3351af
Aybüke Ceren Duran <36036370+cerendrnn@users.noreply.github.com>
2018-05-01 10:02:58 +0300

    Add files via upload

---
+++ src/core/src/com/group1e/tankzone/Entities/Castle.java
@@ -0,0 +1,20 @@
+package com.group1e.tankzone.Entities;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.group1e.tankzone.Components.*;
+
+import static com.badlogic.gdx.math.MathUtils.random;
+
+public class Castle extends Entity{
+
+    public Castle(String faction, String climate, float pos_x, float pos_y,float spawnfrequency){
+
+        Texture texture = new Texture("castle_" + climate + ".png");
+        this.addComponent(new PositionComponent(pos_x, pos_y));
+        this.addComponent(new GraphicsComponent(texture));
+        this.addComponent(new SizeComponent(500));
+        this.addComponent(new HealthComponent(500));
+        this.addComponent(new SpawnComponent(spawnfrequency));
+        this.addComponent(new FactionComponent(faction));
+    }
+}
