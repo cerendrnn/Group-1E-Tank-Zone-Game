17f2f3319115c77bf1c5f7df7c78dcf016653027
PythEch <pythech.tr@gmail.com>
2018-05-05 13:46:41 +0300

    Calculate castle size properly

--- src/core/src/com/group1e/tankzone/Entities/Castle.java
+++ src/core/src/com/group1e/tankzone/Entities/Castle.java
@@ -10,9 +10,11 @@ public class Castle extends Entity{
     public Castle(String faction, String climate, float pos_x, float pos_y,float spawnfrequency){
 
         Texture texture = new Texture("Map/castles/"+ climate + ".png");
+        float size = (texture.getHeight() + texture.getWidth()) / 4.0f;
+
         this.addComponent(new PositionComponent(pos_x, pos_y));
         this.addComponent(new GraphicsComponent(texture));
-        this.addComponent(new SizeComponent(500));
+        this.addComponent(new SizeComponent(size));
         this.addComponent(new HealthComponent(500));
         this.addComponent(new SpawnComponent(spawnfrequency));
         this.addComponent(new FactionComponent(faction));
