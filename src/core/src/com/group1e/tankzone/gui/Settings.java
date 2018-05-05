9ee8d9639dfd63905a466d1d99ab231722cd3e9a
Aybüke Ceren Duran <36036370+cerendrnn@users.noreply.github.com>
2018-05-05 18:18:11 +0300

    Update Settings.java

--- src/core/src/com/group1e/tankzone/gui/Settings.java
+++ src/core/src/com/group1e/tankzone/gui/Settings.java
@@ -2,25 +2,21 @@ package com.group1e.tankzone.gui;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import javax.sound.sampled.FloatControl;
-import java.io.FileReader;
 import java.io.IOException;
-import javax.swing.border.*;
-
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 
 public class Settings extends JPanel {
 
     BufferedImage background = null;
     ImageIcon img = null;
     JButton back;
+    private JTextArea textArea;
+    private JButton yesButton;
+    private JButton noButton;
     Font font;
     File f = null;
 
@@ -28,18 +24,41 @@ public class Settings extends JPanel {
 
     public Settings(){
         //Back button
-        img = new ImageIcon("back.png");
+        textArea = new JTextArea();
+        textArea.setText("\n\n" + "         " +"SOUND ON OR OFF?");
+        textArea.setBorder(new LineBorder(Color.BLACK, 3));
+        textArea.setOpaque(true);
+        textArea.setEditable(false);
+        font = new Font("CALIBRI", Font.BOLD | Font.ITALIC, 25);
+        textArea.setFont(font);
+        textArea.setForeground(Color.BLACK);
+        textArea.setBackground(Color.LIGHT_GRAY);
+        img = new ImageIcon("C:\\Users\\Ceren\\IdeaProjects\\Group-1E-Tank-Zone-Game\\src\\desktop\\assets\\back.png");
         Image temp = img.getImage();
         temp = temp.getScaledInstance(50, 50,  java.awt.Image.SCALE_SMOOTH);
         img = new ImageIcon(temp);
         back = new JButton(img);
         back.setBorderPainted( false );
         back.setBounds(20, 20, 50, 50);
+        textArea.setBounds(350, 200, 350, 150);
 
+        yesButton = new JButton("ON");
+        noButton = new JButton("OFF");
+        yesButton.setFont(new Font("CALIBRI", Font.BOLD | Font.ITALIC, 30));
+        noButton.setFont(new Font("CALIBRI", Font.BOLD | Font.ITALIC, 30));
+        yesButton.setBorder(new LineBorder(Color.BLACK, 3));
+        noButton.setBorder(new LineBorder(Color.BLACK, 3));
+        yesButton.setOpaque(true);
+        noButton.setOpaque(true);
+        yesButton.setBackground(Color.LIGHT_GRAY);
+        noButton.setBackground(Color.LIGHT_GRAY);
 
-
+        //yesButton.setPreferredSize( new Dimension(300, 50));
+        //noButton.setPreferredSize( new Dimension(300, 50));
+        yesButton.setBounds(340,380,80,50);
+        noButton.setBounds(500,380,80,50);
         try {
-            f = new File("settings1.jpg");
+            f = new File("C:\\Users\\Ceren\\IdeaProjects\\Group-1E-Tank-Zone-Game\\src\\desktop\\assets\\settings1.png");
             background = new BufferedImage(627, 626, BufferedImage.TYPE_INT_ARGB);
             background = ImageIO.read(f);
         } catch (IOException e){
@@ -48,7 +67,9 @@ public class Settings extends JPanel {
 
 
 
-
+        this.add(textArea);
+        this.add(yesButton);
+        this.add(noButton);
         this.setPreferredSize(new Dimension(1200, 800));
         this.setLayout( null);
 
